7c5c4fa2db60e6eb5b106d2e1b6df26c4f0cd5ed
incesamsul <ince.samsul89@gmail.com>
2023-08-15 20:55:29 +0800

    fix

--- resources/views/components/sidebar/menu_kasir.blade.php
+++ resources/views/components/sidebar/menu_kasir.blade.php
@@ -6,7 +6,7 @@
 </li>
 
 
-<li class="" id="liTransaksiPos" data-toggle="tooltip" title="Transaksi Pos" data-placement="right">
+<li class="" id="liTransaksiPos" data-toggle="tooltip" title="Laporan Transaksi " data-placement="right">
     <a class="nav-link" href="{{ URL::to('/kasir/transaksi_pos') }}">
         <i class="fa-regular fa-rectangle-list"></i>
         <span>Transaksi Pos</span>
